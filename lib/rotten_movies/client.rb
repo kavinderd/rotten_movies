18071911def54b8dcaa4164f20993f5b73a56ba6
Kavinder Dhaliwal <kavinderd@gmail.com>
2014-06-05 20:48:54 -0700

    tests on client setting base url with version variant

--- lib/rotten_movies/client.rb
+++ lib/rotten_movies/client.rb
@@ -1,7 +1,15 @@
 module RottenMovies
 
   class Client
-  	
+  	BASE_URL = "http://api.rottentomatoes.com/api/public/"
+  	attr_reader :api_key, :base_url
+
+	def initialize(api_key, version="v1.0")
+	  @api_key = api_key
+	  @base_url = BASE_URL + version
+	end 
+	
+
   end
 
 end
--- spec/rotten_movies/client_spec.rb
+++ spec/rotten_movies/client_spec.rb
@@ -3,9 +3,14 @@ require "spec_helper"
 
 describe RottenMovies::Client do 
   
-  it "should initialize with an api key" do
-  	client = RottenMovies::Client.new(api_key: 'test_key')
-  	client.should be_kind_of Client
+  it "should set a default base url" do
+  	client = RottenMovies::Client.new('test_key')
+  	client.base_url.should eq RottenMovies::Client::BASE_URL + 'v1.0'
+  end
+
+  it "should use a passed in version number" do
+    client = RottenMovies::Client.new('test_key', 'v1.1')
+    client.base_url.should eq RottenMovies::Client::BASE_URL + 'v1.1'
   end
 
 end
