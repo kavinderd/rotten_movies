f829ac4e45e9d2133534e9966f3d3868764dff53
Kavinder Dhaliwal <kavinderd@gmail.com>
2014-06-05 20:36:13 -0700

    add vcr config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,11 @@
 require './lib/rotten_movies'
+require 'vcr'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'fixtures/vcr_cassettes'
+  c.hook_into :webmock
+end
+
 RSpec.configure do |config|
 
 
